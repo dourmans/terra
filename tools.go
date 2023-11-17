3d6967bc0c68c9efb116d9cea72aeb2199a974cb
dourmans <142157639+dourmans@users.noreply.github.com>
2023-11-18 06:27:38 +0700

    Update tools.go

--- tools.go
+++ tools.go
@@ -9,7 +9,7 @@ package tools
 // This file tracks some external tools we use during development and release
 // processes. These are not used at runtime but having them here allows the
 // Go toolchain to see that we need to include them in go.mod and go.sum.
-
+s = 7 
 import (
 	_ "github.com/hashicorp/copywrite"
 	_ "github.com/nishanths/exhaustive/cmd/exhaustive"
